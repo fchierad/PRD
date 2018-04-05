fe2ca56753f7109d3a313e522d4f47c6cf8e0a9a
fchierad <fchierad@gmail.com>
2018-04-05 00:10:17 -0600

    added get(), test(), basicHTTPauthHeader(), shouldRetry()

--- PRD.js
+++ PRD.js
@@ -1,8 +1,8 @@
 /**
- * Simple module to assist on workflow development
+ * Simple module to assist on workflow development.
  * Created to be imported in Overview > Global Scripts and used on both forms and engine.
  * @namespace PRD
- * @version 1.0.2
+ * @version 1.0.3
  * @license MIT License
  */
 var PRD = (function IIFE( logprefix ) {
@@ -12,7 +12,12 @@ var PRD = (function IIFE( logprefix ) {
       formOrEngine:formOrEngine,
       logerror:logerror,
       coerceToString:coerceToString,
-      JSONobj:JSONobj
+      JSONobj: {
+        JSONparse:parse,
+        JSONstringify:stringify,
+        JSONget:get,
+        JSONtest:test
+      }
     },
     setlogprefix:setlogprefix,
     version:version,
@@ -21,8 +26,30 @@ var PRD = (function IIFE( logprefix ) {
   };
 
   /**
-   * @namespace util
+   * Return module version.
+   *
    * @memberof PRD
+   * @since 1.0.1
+   *
+   * @type {string}
+   * @return {string} Module's version in the format M.m.p (Major, minor, patch)
+   */
+  function version() {
+    return '1.0.3';
+  }
+
+  /**
+   * @namespace PRD.util
+   * @memberof PRD
+   */
+
+    /**
+   * Proxy for the actual JSON object used in browsers (JSON) or workflow engine (ScriptVault.JSON).
+   * Wraps the parse() and stringify() calls in try/catch blocks to report errors via logerror() instead of
+   * letting the workflow engine break its regular flow when an error occurs.
+   * @namespace PRD.util.JSONobj
+   * @memberof PRD.util
+   * @since 1.0.2
    */
 
   /**
@@ -48,16 +75,21 @@ var PRD = (function IIFE( logprefix ) {
   // Initializes the prefix variable used by the logerror() calls.
   setlogprefix( logprefix );
 
-    /**
-   * Proxy for the actual JSON object used in browsers (JSON) or workflow engine (ScriptVault.JSON).
-   * Wraps the parse() and stringify() calls in try/catch blocks to report errors via logerror() instead of
-   * letting the workflow engine break its regular flow when an error occurs.
+  /**
+   * Wrapper for the parse() call on the host environment's JSON object.
+   * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
    *
-   * @memberof PRD
+   * @function parse
+   * @memberof PRD.util.JSONobj
    * @since 1.0.2
+   *
+   * @param {string}     s          String with valid JSON syntax
+   * @param {function=}  [reviver] (Optional) If a function, this prescribes how the value originally produced by parsing is transformed, before being returned.
+   *
+   * @type {object}
+   * @return {object} ECMA Object generated from the JSON string. On error return empty object and report the error via logerror()
    */
-  function JSONobj() {}
-  JSONobj.parse = function jp( s, r ) {
+  function JSONparse( s, r ) {
     var res, pointer;
     // Setup a pointer to the JSON object since its name differs on forms and engine.
     if ( where === 'engine' ) {
@@ -75,8 +107,24 @@ var PRD = (function IIFE( logprefix ) {
       return {};
     }
     return res;
-  };
-  JSONobj.stringify = function js( o, r, s ) {
+  }
+
+  /**
+   * Wrapper for the stringify() call on the host environment's JSON object.
+   * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+   *
+   * @function stringify
+   * @memberof PRD.util.JSONobj
+   * @since 1.0.2
+   *
+   * @param {object}                        o           ECMA object to convert to JSON
+   * @param {(function|string[]|number[])=} [replacer]  (Optional) A function that alters the behavior of the stringification process, or an array of String and Number objects that serve as a whitelist for selecting/filtering the properties of the value object to be included in the JSON string. If this value is null or not provided, all properties of the object are included in the resulting JSON string
+   * @param {(string|number)=}              [space]     (Optional) A String or Number object that's used to insert white space into the output JSON string for readability purposes. If this is a Number, it indicates the number of space characters to use as white space; this number is capped at 10 (if it is greater, the value is just 10). Values less than 1 indicate that no space should be used. If this is a String, the string (or the first 10 characters of the string, if it's longer than that) is used as white space. If this parameter is not provided (or is null), no white space is used.
+   *
+   * @type {string}
+   * @return {string} Serialized ECMA object in JSON format. On error return empty string and report the error via logerror()
+   */
+  function JSONstringify( o, r, s ) {
     var res, pointer;
     // Setup a pointer to the JSON object since its name differs on forms and engine.
     if ( where === 'engine' ) {
@@ -94,21 +142,207 @@ var PRD = (function IIFE( logprefix ) {
       return '';
     }
     return res;
-  };
-
-  /**
-   * Return module version
-   *
-   * @memberof PRD
-   * @since 1.0.1
-   *
-   * @type {string}
-   * @return {string} Module's version in the format M.m.p (Major, minor, patch)
-   */
-  function version() {
-    return '1.0.2';
   }
 
+  /**
+ * Verify if an ECMA object has the selected location.
+ * Note: To reference properties with a dot in their name use the format ["property.name"] .
+ *
+ * Ported from IDM engine to RBPM. IDM Engine version at https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.js
+ *
+ * @function test
+ * @memberof PRD.util.JSONobj
+ * @since 1.0.3
+ *
+ * @param {(object|string)}  inputJSON    Input JSON (ECMA object). If a string-serialized JSON is provided it will be converted to a JSON object internally
+ * @param {string}           whattotest   Dot-separated list of properties as if you are accessing them via ECMAscript
+ *
+ * @return {boolean} true if the path is found, false otherwise
+ */
+function JSONtest( inputJSON, whattotest ) {
+  var fname, i, itval, itobj, JSONobj, getArr, propName;
+  fname = 'JSONtest(): ';
+  // Review input data
+  if ( typeof inputJSON === 'string' ) {
+    JSONobj = JSONparse( inputJSON );
+  } else {
+    JSONobj = inputJSON;
+  }
+  if ( typeof whattotest === 'string' ) {
+    getArr = charArrToPropertyNames( stringToCharArray( whattotest ) );
+  } else {
+    return false;
+  }
+  // Iterates through the object using itobj and itval as the middle steps to find the desired result
+  itobj = JSONobj;
+  for( i = 0; i < getArr.length; i++ ) {
+    propName = getArr[ i ];
+    if ( typeof itobj === 'object' && propName in itobj ) {
+      itval = itobj[ propName ];
+    } else {
+      return false;
+    }
+    itobj = itval;
+  }
+  return true;
+}
+
+/**
+ * Retrieves a property of an ECMA object (or its subordinate object) and returns it in the specified type.
+ * Note: To reference properties with a dot in their name use the format ["property.name"] .
+ *
+ * Ported from IDM engine to RBPM. IDM Engine version at https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.js
+ *
+ * @function get
+ * @memberof PRD.util.JSONobj
+ * @since 1.0.3
+ *
+ * @param {(object|string)}  inputJSON     Input JSON (ECMA object). If a string-serialized JSON is provided it will be converted to a JSON object internally
+ * @param {string}           whattoget     Dot-separated list of properties as if you are accessing them via ECMAscript
+ * @param {string=}          [returntype]  (Optional) Desired return type. Valid values are: string, number, raw. Defaults to raw in case whatever is provided is not one of the 3 valid options
+ *
+ * @return {(string|number|boolean|object)} Selected property's value in the selected format. If parsing of the object fails returns an empty string
+ */
+function JSONget( inputJSON, whattoget, returntype ) {
+  var fname, i, itval, itobj, JSONobj, getArr, propName, res = '';
+  fname = 'JSONget(): ';
+  // Review input data
+  if ( typeof inputJSON === 'string' ) {
+    JSONobj = JSONparse( inputJSON );
+  } else {
+    JSONobj = inputJSON;
+  }
+  if ( typeof whattoget === 'string' ) {
+    getArr = charArrToPropertyNames( stringToCharArray( whattoget ) );
+  } else {
+    return res;
+  }
+  if ( returntype !== 'string' && returntype !== 'number' && returntype !== 'raw' ) {
+      returntype = 'raw';
+  }
+  // Iterates through the object using itobj and itval as the middle steps to find the desired result
+  itobj = JSONobj;
+  for( i = 0; i < getArr.length; i++ ) {
+    propName = getArr[ i ];
+    if ( typeof itobj === 'object' && propName in itobj ) {
+      itval = itobj[ propName ];
+    } else {
+      return res;
+    }
+    itobj = itval;
+  }
+  // Inspect returned data and coerce it as needed. No default set since res is set at the start of the function
+  switch ( returntype ) {
+    case 'string':
+      res = String( itval );
+      break;
+    case 'number':
+      res = parseInt( String( itval ), 10 );
+      break;
+    case 'raw':
+      res = itval;
+      break;
+  }
+  return res;
+}
+
+//  https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.md#JSONget
+
+  /**
+   * Unicode-safe split of a string to a character Array.
+   *
+   * Since IDM 4.5/IDM 4.6 does not have access to ES6 - therefore no spread ... operator - this function is needed.
+   * Once IDM supports the spread operator use that instead.
+   *
+   * Ported from IDM engine to RBPM. IDM Engine version at https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.js
+   *
+   * @since 1.0.3
+   * @private
+   * @param {string}  str   String input. Any other input will be coerced to string using String() and probably won't behave as expected
+   * @type {string[]}
+   * @return {string[]} Unicode-safe character array
+   */
+  function stringToCharArray( str ) {
+    var cArr = [], fname;
+    fname = 'stringToCharArray(): ';
+    try {
+      cArr = String( str ).split( /(?=(?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/ );
+    } catch( e ) {
+      logerror( fname + 'Failed to split the input string, error: ' + e.message );
+    }
+    return cArr;
+  }
+
+  /**
+   * JSON path parser. Iterates through a character array and returns an array with property names.
+   * Array indexes are purely numeric property names and will be returned as numbers, not strings.
+   * Character Array should have been split from the original ECMA object path that we want to parse.
+   *
+   * Ported from IDM engine to RBPM. IDM Engine version at https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.js
+   *
+   * @since 1.0.0
+   * @private
+   * @param {string[]}  str   Character string. Assumes each entry in the array is a single Unicode character
+   * @type {Array<(string|number)>}
+   * @return {Array<(string|number)>} property names array
+   */
+  function charArrToPropertyNames( cArr ) {
+    var i, fname, currentName, squaremark, re_whitespace, re_number, re_quotes, property = [];
+    fname = 'charArrToPropertyNames(): ';
+    if ( !( cArr instanceof Array ) ) {
+      logerror( fname + 'Input parameter is not an Array, aborting.' );
+      return property;
+    }
+    // Setup for parsing. Delimiter for property names are either dot or open and close square brackets
+    // If the contents inside square brackets are purely numeric then a number is returned
+    currentName = '';
+    re_number = /^\d+$/;
+    re_quotes = /^(['"])(.+)\1$/;
+    squaremark = ( cArr[ 0 ] === '[' )? 'first':'end'; //squaremark can be 'first', 'start', 'end'
+    // Iterates through the character array parsing elements into their own property array entry
+    for( i=0; i < cArr.length; i++ ) {
+      if ( squaremark === 'first' && cArr[ i ] === '[' ) {
+        squaremark = 'start';
+        continue;
+      }
+      if ( squaremark === 'end' && cArr[ i ] === '[' ) {
+        squaremark = 'start';
+        if ( currentName.trim() !== '' ) { // prevent double push on constructs like arr[0][0]
+          property.push( currentName.trim() );
+        }
+        currentName = '';
+        continue;
+      }
+      if ( squaremark === 'start' && cArr[ i ] === ']' ) {
+        squaremark = 'end';
+        currentName = currentName.trim();
+        // If the property name between [] is a pure number, coerces the string to a number
+        if ( re_number.test( currentName ) ) {
+          currentName = parseInt( currentName, 10 );
+        }
+        // Remove quotes around property name if they are present like obj["property name"], returning property name
+        if ( re_quotes.test( currentName ) ) {
+          currentName = re_quotes.exec( currentName )[2];
+        }
+        property.push( currentName );
+        currentName = '';
+        continue;
+      }
+      // Traditional . delimiter
+      if ( squaremark === 'end' && cArr[ i ] === '.' ) {
+        if ( currentName.trim() !== '' ) { // prevent double push on constructs like arr[0].name
+          property.push( currentName.trim() );
+        }
+        currentName = '';
+        continue;
+      }
+      currentName += cArr[ i ];
+    }
+    if ( currentName !== '' ) {
+      property.push( currentName.trim() );
+    }
+    return property;
+  }
 
   /**
    * Set prefix for the log activities generated by logerror() on both engine and form.
@@ -258,6 +492,52 @@ var PRD = (function IIFE( logprefix ) {
       ret = String( input.join( ' ' ) );
     }
     return ret;
+  }
+
+  /**
+   * (Engine only) Returns basic HTTP auth header. Format is: Basic B64encodedUSERNAME:PASSWORD
+   *
+   * @memberof PRD.util
+   * @since 1.0.3
+   *
+   * @param {string}  username  HTTP auth user's name.
+   * @param {string}  password  HTTP auth user's password.
+   *
+   * @type {string}
+   * @return {string} HTTP Basic 'Authorization' header's value.
+   */
+  function basicHTTPauthHeader( username, password ) {
+    var res = 'Basic ', conc, JString, Base64, b64str;
+    try {
+      JString = java.lang.String;
+      Base64 = new Packages.org.apache.commons.codec.binary.Base64();
+      conc = JString( String( username ) + ':' + String( password ) ).getBytes( 'UTF-8' );
+      b64str = String( JString( Base64.encodeBase64( conc ), 'UTF-8' ) );
+      res += b64str;
+    } catch( e ) {
+      logerror( 'basicAuthHeader(): Failed to build Basic Authentication HTTP header value. Error: ' + e.message );
+    }
+    return res;
+  }
+
+  /**
+   * (Engine only) Function to coerce provided flowdata and return if we are past max retries or not.
+   *
+   * @memberof PRD.util
+   * @since 1.0.3
+   *
+   * @param {string}  curcounter  Current Counter
+   * @param {string}  maxretries  Maximum retries value
+   *
+   * @type {boolean}
+   * @return {boolean} true if maxretries is greater than current counter, false otherwise.
+   */
+  function shouldRetry( curcounter, maxretries ) {
+    var retry = false;
+    curcounter = String( curcounter ) | 0;
+    maxretries = String( maxretries ) | 0;
+    retry = ( curcounter < maxretries ) ? true : false;
+    return retry;
   }
 
   /**
@@ -834,6 +1114,8 @@ var PRD = (function IIFE( logprefix ) {
     PublicAPI.util.Unique = Unique;
     PublicAPI.util.Compare = Compare;
     PublicAPI.util.getObject2arr = getObject2arr;
+    PublicAPI.util.basicHTTPauthHeader = basicHTTPauthHeader;
+    PublicAPI.util.shouldRetry = shouldRetry;
   }
   // Form-only API extensions:
   if ( where === 'form' ) {
--- PRD.md
+++ PRD.md
@@ -4,14 +4,20 @@
 Simple module to assist on workflow developmentCreated to be imported in Overview > Global Scripts and used on both forms and engine.
 
 **Kind**: global namespace  
-**Version**: 1.0.2  
+**Version**: 1.0.3  
 **License**: MIT License  
 
 * [PRD](#PRD) : <code>object</code>
     * [.util](#PRD.util) : <code>object</code>
+        * [.JSONobj](#PRD.util.JSONobj) : <code>object</code>
+            * [.parse(s, [reviver])](#PRD.util.JSONobj.parse) ⇒ <code>object</code>
+            * [.stringify(o, [replacer], [space])](#PRD.util.JSONobj.stringify) ⇒ <code>string</code>
+            * [.test(inputJSON, whattotest)](#PRD.util.JSONobj.test) ⇒ <code>boolean</code>
         * [.formOrEngine()](#PRD.util.formOrEngine) ⇒ <code>string</code>
         * [.logerror(msg)](#PRD.util.logerror) ⇒ <code>boolean</code>
         * [.coerceToString(input, [defVal])](#PRD.util.coerceToString) ⇒ <code>string</code>
+        * [.basicHTTPauthHeader(username, password)](#PRD.util.basicHTTPauthHeader) ⇒ <code>string</code>
+        * [.shouldRetry(curcounter, maxretries)](#PRD.util.shouldRetry) ⇒ <code>boolean</code>
         * [.Compare(list1, list2, ignorecase)](#PRD.util.Compare) ⇒ <code>Array.&lt;Array.&lt;string&gt;&gt;</code>
         * [.Unique(obj, ignorecase)](#PRD.util.Unique) ⇒ <code>Array.&lt;string&gt;</code> \| <code>java.util.Vector</code>
         * [.getObject2arr(list)](#PRD.util.getObject2arr) ⇒ <code>array.&lt;(object\|string)&gt;</code> \| <code>null</code>
@@ -19,7 +25,6 @@ Simple module to assist on workflow developmentCreated to be imported in Overvi
         * [.ECMAArrayToJavaVector(arr)](#PRD.util.ECMAArrayToJavaVector) ⇒ <code>java.util.Vector</code>
         * [.GCVget(GCVname, returnType, DefaultValue)](#PRD.util.GCVget) ⇒ <code>string</code> \| <code>number</code> \| <code>boolean</code>
         * [.getNamedPassword(NamedPassword, try5times)](#PRD.util.getNamedPassword) ⇒ <code>string</code> \| <code>null</code>
-    * [.JSONobj()](#PRD.JSONobj)
     * [.version()](#PRD.version) ⇒ <code>string</code>
     * [.setlogprefix(str)](#PRD.setlogprefix)
     * [.init(field, form, IDVault)](#PRD.init)
@@ -32,9 +37,15 @@ Simple module to assist on workflow developmentCreated to be imported in Overvi
 **Kind**: static namespace of [<code>PRD</code>](#PRD)  
 
 * [.util](#PRD.util) : <code>object</code>
+    * [.JSONobj](#PRD.util.JSONobj) : <code>object</code>
+        * [.parse(s, [reviver])](#PRD.util.JSONobj.parse) ⇒ <code>object</code>
+        * [.stringify(o, [replacer], [space])](#PRD.util.JSONobj.stringify) ⇒ <code>string</code>
+        * [.test(inputJSON, whattotest)](#PRD.util.JSONobj.test) ⇒ <code>boolean</code>
     * [.formOrEngine()](#PRD.util.formOrEngine) ⇒ <code>string</code>
     * [.logerror(msg)](#PRD.util.logerror) ⇒ <code>boolean</code>
     * [.coerceToString(input, [defVal])](#PRD.util.coerceToString) ⇒ <code>string</code>
+    * [.basicHTTPauthHeader(username, password)](#PRD.util.basicHTTPauthHeader) ⇒ <code>string</code>
+    * [.shouldRetry(curcounter, maxretries)](#PRD.util.shouldRetry) ⇒ <code>boolean</code>
     * [.Compare(list1, list2, ignorecase)](#PRD.util.Compare) ⇒ <code>Array.&lt;Array.&lt;string&gt;&gt;</code>
     * [.Unique(obj, ignorecase)](#PRD.util.Unique) ⇒ <code>Array.&lt;string&gt;</code> \| <code>java.util.Vector</code>
     * [.getObject2arr(list)](#PRD.util.getObject2arr) ⇒ <code>array.&lt;(object\|string)&gt;</code> \| <code>null</code>
@@ -42,6 +53,62 @@ Simple module to assist on workflow developmentCreated to be imported in Overvi
     * [.ECMAArrayToJavaVector(arr)](#PRD.util.ECMAArrayToJavaVector) ⇒ <code>java.util.Vector</code>
     * [.GCVget(GCVname, returnType, DefaultValue)](#PRD.util.GCVget) ⇒ <code>string</code> \| <code>number</code> \| <code>boolean</code>
     * [.getNamedPassword(NamedPassword, try5times)](#PRD.util.getNamedPassword) ⇒ <code>string</code> \| <code>null</code>
+
+<a name="PRD.util.JSONobj"></a>
+
+#### util.JSONobj : <code>object</code>
+Proxy for the actual JSON object used in browsers (JSON) or workflow engine (ScriptVault.JSON).Wraps the parse() and stringify() calls in try/catch blocks to report errors via logerror() instead ofletting the workflow engine break its regular flow when an error occurs.
+
+**Kind**: static namespace of [<code>util</code>](#PRD.util)  
+**Since**: 1.0.2  
+
+* [.JSONobj](#PRD.util.JSONobj) : <code>object</code>
+    * [.parse(s, [reviver])](#PRD.util.JSONobj.parse) ⇒ <code>object</code>
+    * [.stringify(o, [replacer], [space])](#PRD.util.JSONobj.stringify) ⇒ <code>string</code>
+    * [.test(inputJSON, whattotest)](#PRD.util.JSONobj.test) ⇒ <code>boolean</code>
+
+<a name="PRD.util.JSONobj.parse"></a>
+
+##### JSONobj.parse(s, [reviver]) ⇒ <code>object</code>
+Wrapper for the parse() call on the host environment's JSON objectSee https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+
+**Kind**: static method of [<code>JSONobj</code>](#PRD.util.JSONobj)  
+**Returns**: <code>object</code> - ECMA Object generated from the JSON string. On error return empty object and report the error via logerror()  
+**Since**: 1.0.2  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| s | <code>string</code> | String with valid JSON syntax |
+| [reviver] | <code>function</code> | (Optional) If a function, this prescribes how the value originally produced by parsing is transformed, before being returned. |
+
+<a name="PRD.util.JSONobj.stringify"></a>
+
+##### JSONobj.stringify(o, [replacer], [space]) ⇒ <code>string</code>
+Wrapper for the stringify() call on the host environment's JSON objectSee https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+
+**Kind**: static method of [<code>JSONobj</code>](#PRD.util.JSONobj)  
+**Returns**: <code>string</code> - Serialized ECMA object in JSON format. On error return empty string and report the error via logerror()  
+**Since**: 1.0.2  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| o | <code>object</code> | ECMA object to convert to JSON |
+| [replacer] | <code>function</code> \| <code>Array.&lt;string&gt;</code> \| <code>Array.&lt;number&gt;</code> | (Optional) A function that alters the behavior of the stringification process, or an array of String and Number objects that serve as a whitelist for selecting/filtering the properties of the value object to be included in the JSON string. If this value is null or not provided, all properties of the object are included in the resulting JSON string |
+| [space] | <code>string</code> \| <code>number</code> | (Optional) A String or Number object that's used to insert white space into the output JSON string for readability purposes. If this is a Number, it indicates the number of space characters to use as white space; this number is capped at 10 (if it is greater, the value is just 10). Values less than 1 indicate that no space should be used. If this is a String, the string (or the first 10 characters of the string, if it's longer than that) is used as white space. If this parameter is not provided (or is null), no white space is used. |
+
+<a name="PRD.util.JSONobj.test"></a>
+
+##### JSONobj.test(inputJSON, whattotest) ⇒ <code>boolean</code>
+Verify if an ECMA object has the selected location.Note: To reference properties with a dot in their name use the format ["property.name"]Ported from IDM engine to RBPM. IDM Engine version at https://github.com/fchierad/IDM-ECMAlib/blob/v1.0.2/JSONlib-JS.js
+
+**Kind**: static method of [<code>JSONobj</code>](#PRD.util.JSONobj)  
+**Returns**: <code>boolean</code> - true if the path is found, false otherwise  
+**Since**: 1.0.3  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| inputJSON | <code>object</code> \| <code>string</code> | Input JSON (ECMA object). If a string-serialized JSON is provided it will be converted to a JSON object internally |
+| whattotest | <code>string</code> | Dot-separated list of properties as if you are accessing them via ECMAscript |
 
 <a name="PRD.util.formOrEngine"></a>
 
@@ -77,6 +144,34 @@ Coerces the result to an ECMA string.Coercion rules for this function:If the f
 | --- | --- | --- |
 | input | <code>any</code> | Input parameter to be coerced. |
 | [defVal] | <code>string</code> | Default value to be used |
+
+<a name="PRD.util.basicHTTPauthHeader"></a>
+
+#### util.basicHTTPauthHeader(username, password) ⇒ <code>string</code>
+(Engine only) Returns basic HTTP auth header. Format is: Basic B64encodedUSERNAME:PASSWORD
+
+**Kind**: static method of [<code>util</code>](#PRD.util)  
+**Returns**: <code>string</code> - HTTP Basic 'Authorization' header's value.  
+**Since**: 1.0.3  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| username | <code>string</code> | HTTP auth user's name. |
+| password | <code>string</code> | HTTP auth user's password. |
+
+<a name="PRD.util.shouldRetry"></a>
+
+#### util.shouldRetry(curcounter, maxretries) ⇒ <code>boolean</code>
+(Engine only) Function to coerce provided flowdata and return if we are past max retries or not.
+
+**Kind**: static method of [<code>util</code>](#PRD.util)  
+**Returns**: <code>boolean</code> - true if maxretries is greater than current counter, false otherwise.  
+**Since**: 1.0.3  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| curcounter | <code>string</code> | Current Counter |
+| maxretries | <code>string</code> | Maximum retries value |
 
 <a name="PRD.util.Compare"></a>
 
@@ -175,13 +270,6 @@ Coerces the result to an ECMA string.Coercion rules for this function:If the f
 | NamedPassword | <code>string</code> | Name of the GCV-ref to the Named Password to be retrieved. |
 | try5times | <code>any</code> | If not provided or null a single read is attempted.                                 If provides and read fails try 4 more times, with 1 second pause between them. |
 
-<a name="PRD.JSONobj"></a>
-
-### PRD.JSONobj()
-Proxy for the actual JSON object used in browsers (JSON) or workflow engine (ScriptVault.JSON).Wraps the parse() and stringify() calls in try/catch blocks to report errors via logerror() instead ofletting the workflow engine break its regular flow when an error occurs.
-
-**Kind**: static method of [<code>PRD</code>](#PRD)  
-**Since**: 1.0.2  
 <a name="PRD.version"></a>
 
 ### PRD.version() ⇒ <code>string</code>
